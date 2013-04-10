4f830cd1bc0646dd4cfd40d30229efdae9c936b6
Derek Parker <parkerderek86@gmail.com>
2013-04-09 20:10:47 -0500

    Shaved off 2 seconds... still not satisfied

--- summation_of_primes/summation_of_primes.rb
+++ summation_of_primes/summation_of_primes.rb
@@ -1,53 +1,17 @@
-require 'prime'
+require 'mathn'
 
 module SummationOfPrimesSolver
-  attr_reader :limit
+  LIMIT = 2_000_000
 
   extend self
-  def solve(limit)
-    @limit = limit
-
-    solution
-  end
-
-  private
-
-  def solution
-    range.each do
-      if last_prime > limit
-        primes.pop
-        break
-      end
-
-      primes << generate_next_prime
+  def solve(limit = LIMIT)
+    Prime.inject do |sum, prime|
+      return sum if prime > limit
+      sum + prime
     end
-
-    primes_sum
-  end
-
-  def range
-    (1..Float::INFINITY).lazy
-  end
-
-  def primes_sum
-    primes.reduce(:+)
-  end
-
-  def primes
-    @range ||= []
-  end
-
-  def last_prime
-    primes.last || 1
-  end
-
-  def prime_generator
-    @prime_generator ||= Prime::EratosthenesGenerator.new
-  end
-
-  def generate_next_prime
-    prime_generator.succ
   end
 end
 
-p SummationOfPrimesSolver.solve(2000000)
+if $0 == __FILE__
+  p SummationOfPrimesSolver.solve
+end
