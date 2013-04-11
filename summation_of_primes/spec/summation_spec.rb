3e805451d4de4b9966adc9b3ccbdfec5272e7a55
Derek Parker <parkerderek86@gmail.com>
2013-04-10 19:30:41 -0500

    Move old folders

--- summation_of_primes/spec/summation_spec.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../summation_of_primes'
-
-describe SummationOfPrimesSolver do
-  context "primes under 10" do
-    it "should equal 17" do
-      SummationOfPrimesSolver.solve(10).should eq 17
-    end
-  end
-end
